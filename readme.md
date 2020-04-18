2b9c2a5b88f0479fe005aebaaa185169bed63662
Krishna Teja Vaddepalli <kvadpal@gmail.com>
2020-04-18 09:37:00 +0300

    https support

---
+++ readme.md
@@ -0,0 +1,9 @@
+Base code is taken from https://github.com/Vinnu1/simple-videochat-webrtc/
+
+Needs https to run. Modified accordingly
+
+To run
+```
+npm install
+npm start
+```
---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const app = express()
+const fs = require('fs')
+const options = {
+  key: fs.readFileSync('SSL.key'),
+  cert: fs.readFileSync('SSL.crt')
+};
+const https = require('https')
+var server=https.createServer(options, app);
+server.listen(443,function(){
+console.log('Server running on port 3000')
+});
+const http = require('http').Server(app)
+const io = require('socket.io')(server)
+const port = process.env.PORT || 3000 
+
+app.use(express.static(__dirname + "/public"))
+let clients = 0
+
+io.on('connection', function (socket) {
+    socket.on("NewClient", function () {
+	    console.log(clients)
+        if (clients < 2) {
+            if (clients == 1) {
+                this.emit('CreatePeer')
+            }
+        }
+        else
+            this.emit('SessionActive')
+        clients++;
+    })
+    socket.on('Offer', SendOffer)
+    socket.on('Answer', SendAnswer)
+    socket.on('disconnect', Disconnect)
+})
+
+function Disconnect() {
+    if (clients > 0) {
+        if (clients <= 2)
+            this.broadcast.emit("Disconnect")
+        clients--
+    }
+}
+
+function SendOffer(offer) {
+    this.broadcast.emit("BackOffer", offer)
+}
+
+function SendAnswer(data) {
+    this.broadcast.emit("BackAnswer", data)
+}
+
+//https.listen(443, () => console.log(`Active on ${port} port`))
+/*
+var server=https.createServer(options, app);
+server.listen(443,function(){
+console.log('Server running on port 3000')
+});
+*/
